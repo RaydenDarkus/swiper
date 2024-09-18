6105db493cb396571a309b711357bc436f359566
RaydenDarkus <svpatil20000@gmail.com>
2024-09-18 10:06:08 -0400

    commit

--- src/components/MySwiper/MySwiper.jsx
+++ src/components/MySwiper/MySwiper.jsx
@@ -34,7 +34,7 @@ export default function MySwiper() {
 
     const fetchImages = async () => {
       try {
-        const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=7BdaDaLN7EHQyb8Db3NDkE1dPSniiIG2oE0wvt64&hd=True&count=5');
+        const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&hd=True&count=5');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
